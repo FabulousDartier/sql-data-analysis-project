57ab583983fb04780c8dfca2da88e535b6364f98
FabulousDartier <71672859+FabulousDartier@users.noreply.github.com>
2025-07-30 01:39:27 +0100

    Create 10_part_to_whole.sql

---
+++ scripts/10_part_to_whole.sql
@@ -0,0 +1,39 @@
+/*
+===============================================================
+Part-To-Whole Analysis
+===============================================================
+Purpose:
+- To compare performance or metrics across dimensions or time periods.
+- To evaluate differences between categories.
+- Useful for A/B testing or regional comparisons.
+
+SQL Functions Used:
+- SUM(), AVG(): Aggregate values for comparison.
+- Window Functions: SUM() OVER() for total calculations.
+===============================================================
+*/
+
+-- PART-TO-WHOLE Analysis
+-- Which categories contribute the most to overall sales?
+WITH category_sales AS (
+    SELECT 
+        category,
+        SUM(sales_amount) AS total_sales
+    FROM gold.fact_sales AS f
+    LEFT JOIN gold.dim_products AS p 
+        ON p.product_key = f.product_key
+    GROUP BY category
+)
+
+SELECT
+    category,
+    total_sales,
+    SUM(total_sales) OVER() AS overall_sales,
+    CONCAT(
+        ROUND(
+            (CAST(total_sales AS FLOAT) / SUM(total_sales) OVER()) * 100, 2
+        ),
+        '%'
+    ) AS percentage_of_total
+FROM category_sales
+ORDER BY percentage_of_total DESC;
